87c95654235e16cb44cd214fea14c8d7bb6d55e2
COPSTA <tutomx16@gmail.com>
2024-04-18 16:02:03 -0500

    Añade funcion de calculo suma

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 /**VARIABLES */
 
-const range = (start,end) => Array(end - start + 1).fill(start).map((element,index)=> element + index); //Crea un array de numeeros 
+//Funcion suma
+const sum = (nums) =>
+  nums.reduce((acomulador, valorActual) => acomulador + valorActual, 0);
 
-const charRange = (start,end) => 
-range(start.charCodeAt(0),end.charCodeAt(0)).map(code => String.fromCharCode(code)); //Rango de letras
+//funcion generar rango de numero
+const range = (start, end) =>
+  Array(end - start + 1)
+    .fill(start)
+    .map((element, index) => element + index); //Crea un array de numeros
+
+//funcion generar rango de letras
+const charRange = (start, end) =>
+  range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
+    String.fromCharCode(code)
+  ); //Rango de letras
 
 window.onload = () => {
- 
   const container = document.getElementById("container");
   const createLabel = (name) => {
     const label = document.createElement("div"); //Crea una variable la cual es un elemento div
@@ -14,21 +24,16 @@ window.onload = () => {
     label.textContent = name; // le agrega contenido al elemento con el paramentro de la funcion interna
     container.appendChild(label); // agrega el elmento creado al div container
   };
-  const letters = charRange("A","J");
-  letters.forEach(createLabel)
-  range(1,99).forEach(number =>{
-    createLabel(number)
-    letters.forEach(letter => {
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach((number) => {
+    createLabel(number);
+    letters.forEach((letter) => {
       const input = document.createElement("input");
-      input.type ="text";
+      input.type = "text";
       input.id = letter + number;
       input.ariaLabel = letter + number;
-      container.appendChild(input)
+      container.appendChild(input);
     });
   });
 };
-
-
-
-
-
